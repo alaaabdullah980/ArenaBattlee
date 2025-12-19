2237d41197833c634f8e3b0779ac1b8b9b9328fa
Mega Store <alaaabdullah576@gmail.cpm>
2025-12-19 15:38:23 +0200

    Add Warrior.java

--- src/Warrior.java
+++ src/Warrior.java
@@ -1,5 +1,5 @@
 public class Warrior extends Fighter{
     public Warrior(double x,double y, int health,int width , int height,Weapon weapon , Boolean isLeftside) {
-        super(x,y,100,120,60,weapon,isLeftside);
+        super (x,y,100,120,60,weapon,isLeftside);
     }
 }
